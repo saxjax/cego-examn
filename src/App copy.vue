e86ab0c882a5db09093c9476a72d65a9c8224f7d
jav@bankdata.dk <jav@bankdata.dk>
2024-12-01 00:55:57 +0100

    initial setup

---
+++ src/App copy.vue
@@ -0,0 +1,80 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+export default {
+  data() {
+  return {
+    firstName:'saxjaxy',
+    FIRSTNAME:0,
+    lastName:'jaxy',
+    LASTNAME:1,
+    phoneNumber:'000',
+    PHONE:2,
+
+    items:new Map([['bytecoder',['Coder','Byte','8885559999']]]),
+  }
+},
+methods:{
+  submit(){
+    this.items.set(this.lastName.toLocaleLowerCase()+this.firstName.toLocaleLowerCase(),[this.firstName,this.lastName,this.phoneNumber])
+  },
+  sortedList(){
+    
+    return Array.from(
+      this.items)
+      .sort(
+        (a,b) => 
+        a[0].toLowerCase()
+        .localeCompare(
+          b[0].toLowerCase()
+        )
+  
+      );
+  }
+
+
+}
+}
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+<h1>{{firstName}}</h1>
+<p>{{ items }}</p>
+<div>
+  <input v-model="firstName"/>
+  <input v-model="lastName"/>
+  <input v-model="phoneNumber"/>
+  <button v-on:click="submit">Submit</button>
+</div>
+
+<table>
+  <thead>
+    <tr>
+      <td>first name</td>
+      <td>last name</td>
+      <td>phone</td>
+    </tr>
+  </thead>
+  <tbody>
+  <tr v-for="(item) of this.sortedList()" :key="item">
+    <td>{{ item[1][FIRSTNAME] }}</td>
+    <td>{{ item[1][LASTNAME] }}</td>
+    <td>{{ item[1][PHONE] }}</td>
+  </tr>
+</tbody>
+</table>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name:'App',
+  data() {
+  return {
+    status:'Next player X',
+    activePlayer: 'X'
+  }
+},
+methods:{
+  onClick(id,player){
+    this.togglePlayer()
+    console.log(id,player);
+  },
+
+  togglePlayer(){
+    this.activePlayer === 'X' ? this.activePlayer = 'O' : this.activePlayer = 'X'
+    this.status = 'Next player ' + this.activePlayer
+    console.log(this.status)
+  }
+  
+}
+}
+
+</script>
+
+<template>
+  <div id="app">
+    <div class="status">{{ status }}</div>
+    <button>Reset</button>
+      <template class="board" v-for="row in 3" :key="row">
+        <div class="row">
+          <button v-for="button in 3" :key="button" v-on:click="onClick([row,button],activePlayer)" class="square" style="width: 40px;height: 40px;"></button>
+        </div>
+      </template>
+  </div>
+</template>
+
+
+
+<style scoped>
+#app{
+  display: block;
+}
+.board{
+  display:flex
+}
+</style>
