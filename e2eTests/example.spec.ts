03aecf90e3ba8806452b168152d9566979e64b0e
jav@bankdata.dk <jav@bankdata.dk>
2024-12-01 15:22:23 +0100

    e2e test

--- e2eTests/example.spec.ts
+++ e2eTests/example.spec.ts
@@ -1,18 +1,48 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("3 in row wins", async({page})=>{
+  await page.goto('http://localhost:5173/');
+  await expect(page.locator('body')).toContainText('Next player X');
+  await page.locator('.square').first().click();
+  await page.locator('div:nth-child(4) > button').first().click();
+  await page.locator('div').filter({ hasText: /^X$/ }).getByRole('button').nth(1).click();
+  await page.locator('div').filter({ hasText: /^O$/ }).getByRole('button').nth(1).click();
+  await page.locator('div').filter({ hasText: /^XX$/ }).getByRole('button').nth(2).click();
+  await expect(page.locator('body')).toContainText('X has won!');
+}),
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test("3 in collumn wins", async({page})=>{
+await page.goto('http://localhost:5173/');
+await page.locator('.row > button:nth-child(2)').first().click();
+await page.locator('div').filter({ hasText: /^X$/ }).getByRole('button').first().click();
+await page.locator('div:nth-child(4) > button:nth-child(2)').click();
+await page.locator('div').filter({ hasText: /^X$/ }).getByRole('button').nth(2).click();
+await page.locator('div:nth-child(5) > button:nth-child(2)').click();
+await expect(page.locator('body')).toContainText('X has won!');
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+}),
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test("3 in diagonal wins", async({page})=>{
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('button', { name: 'Reset' }).click();
+  await page.locator('.square').first().click();
+  await page.locator('div').filter({ hasText: /^O$/ }).getByRole('button').nth(1).click();
+  await page.locator('div:nth-child(4) > button:nth-child(2)').click();
+  await page.locator('div').filter({ hasText: /^O$/ }).getByRole('button').nth(2).click();
+  await page.locator('div:nth-child(5) > button:nth-child(3)').click();
+  await expect(page.locator('body')).toContainText('O has won!');
+}),
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("tie", async ({page}) => {
+  await page.goto('http://localhost:5173/');
+  await page.locator('.square').first().click();
+  await page.locator('div:nth-child(4) > button:nth-child(2)').click();
+  await page.locator('div').filter({ hasText: /^O$/ }).getByRole('button').nth(2).click();
+  await page.locator('div:nth-child(5) > button:nth-child(2)').click();
+  await page.locator('div').filter({ hasText: /^X$/ }).getByRole('button').nth(1).click();
+  await page.locator('div').filter({ hasText: /^XX$/ }).getByRole('button').nth(2).click();
+  await page.locator('div').filter({ hasText: /^O$/ }).getByRole('button').nth(2).click();
+  await page.locator('div:nth-child(4) > button').first().click();
+  await page.locator('div').filter({ hasText: /^OX$/ }).getByRole('button').first().click();
+  await expect(page.locator('body')).toContainText('Tie, end of game, clict reset');
 });
